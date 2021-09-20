672544f428eaae432c5651325fd63229f40e9c85
khatts <khatts@csc4252021.ssdt.berea.edu>
2021-09-20 14:19:33 -0400

    answers and program

---
+++ coding.7/memory-user.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char *argv[]){
+	int bytes = arg[1];
+	int ma = malloc(sizeof(bytes));
+
+	while(true){
+		for(int i=0; i < bytes; i++){
+			ma[i];
+		}
+	}
+	return 0;
+
+}
