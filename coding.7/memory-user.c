3c4860214e218f781620811ea2f276a7e86d4fd2
khatts <khatts@csc4252021.ssdt.berea.edu>
2021-09-21 22:28:30 -0400

    debugged code and completed questions

--- coding.7/memory-user.c
+++ coding.7/memory-user.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 int main(int argc, char *argv[]){
-	int bytes = arg[1];
-	int ma = malloc(sizeof(bytes));
-
-	while(true){
-		for(int i=0; i < bytes; i++){
-			ma[i];
-		}
-	}
+	int bytes =(atoi(argv[0]))*1024*1024; //convert MB to bytes
+	int* allocation = malloc(bytes);
+	
+	
+	for(int i=0; i<bytes; i++){
+		allocation[i] = i;
+	};	
+	printf("End of Program\n");
+	printf("PID: %d\n",getpid());
 	return 0;
-
 }
